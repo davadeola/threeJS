351bc405d69d84fd2c2fd6fa179cc40f7cd0151e
davabim <adeoladavid15@gmail.com>
2019-06-21 16:45:59 +0300

    Added a render element

--- js/sceneHello.js
+++ js/sceneHello.js
@@ -1,4 +1,11 @@
 var scene = new THREE.Scene();
-var camera  = new THREE.PerspectiveCamera{
-  
-}
+var camera  = new THREE.PerspectiveCamera(
+  75, //field of view
+  window.innerWidth/window.innerHeight,
+  0.1,
+  1000
+);
+
+var renderer = new THREE.webGLRenderer({antialias:true});
+renderer.setClearColor("#e5e5e5");//background setClearColor
+renderer.setSize(window.innerWidth, window.innerHeight);
