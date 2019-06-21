e76633db5bc153d910e05905e345fe9070ec7a4a
davabim <adeoladavid15@gmail.com>
2019-06-21 17:04:51 +0300

    Created a new object called mesh which is a circle

--- js/sceneHello.js
+++ js/sceneHello.js
@@ -15,7 +15,18 @@ document.body.appendChild(renderer.domElement); //Adds the renderer to the DOM
 window.addEventListener("resize", ()=>{
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth/window.innerHeight;
-  camera.updateProjectMatrix();
+  camera.updateProjectionMatrix();
 });
 
 renderer.render(scene, camera);
+
+var geometry = new THREE.SphereGeometry(
+  1, //radius
+  10, //width
+  10 //heigth
+)
+
+var material = new THREE.MeshLambertMaterial({color: 0xed4});
+var mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
