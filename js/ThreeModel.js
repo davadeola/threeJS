55901474f4171a646b8f8814abd670365d54a9ba
davadeola <adeoladavid15@gmail.com>
2019-07-02 20:53:19 +0300

    Added point lights to the image

--- js/ThreeModel.js
+++ js/ThreeModel.js
@@ -18,6 +18,22 @@ var init=()=>{
   directionalLight.castShadow =true;
   scene.add(directionalLight);
 
+  light = new THREE.PointLight(0xc4c4c4, 10);
+  light.position.set(0,300, 500);
+  scene.add(light);
+
+  light2 = new THREE.PointLight(0xc4c4c4, 10);
+  light2.position.set(500,100, 0);
+  scene.add(light2);
+
+  light3 = new THREE.PointLight(0xc4c4c4, 10);
+  light3.position.set(0,100, -500);
+  scene.add(light3);
+
+  light4 = new THREE.PointLight(0xc4c4c4, 10);
+  light.position.set(-500,300,0);
+  scene.add(light4);
+
   var renderer= new THREE.WebGLRenderer({antialias: true});
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
