a8d2417be982177bbb66a0b9294353c7d1a07a1d
davadeola <adeoladavid15@gmail.com>
2019-07-02 20:29:13 +0300

    Placed the gltf in the DOM

--- js/ThreeModel.js
+++ js/ThreeModel.js
@@ -2,22 +2,32 @@
 let scene, camera, renderer;
 var init=()=>{
   scene =new THREE.Scene();
-  scene.background = new THREE.Color(0xfdd);
+  scene.background = new THREE.Color(0xe5e5e5);
 
   camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 5000);
+  camera.rotation.y= 45/180*Math.PI;
+  camera.position.x=800;
+  camera.position.y=100;
+  camera.position.z=1000;
+
   var hLight= new THREE.AmbientLight(0x404040, 100);
   scene.add(hLight);
 
-  renderer= new THREE.WebGLRenderer({antialias: true});
+  var renderer= new THREE.WebGLRenderer({antialias: true});
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+
+  let loader= new THREE.GLTFLoader();
+  loader.load('./../scene.gltf', function(gltf){
+    //get the car
+    car = gltf.scene.children[0];
+
+    car.scale.set(0.5, 0.5, 0.5);
+    scene.add(gltf.scene);
+    renderer.render(scene, camera);
+  });
 }
 
 //Call the THREE.JS GTF LOADER class
-let loader= new THREE.GLTFLoader();
-loader.load('../merc/scene.gltf', (gltf)=>{
-  scene.add(gltf.scene);
-  renderer.render(scene, camera);
-})
 
 init();
